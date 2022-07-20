074d4b153fd860a7774c07e6c116bf1b0ced5e10
djurayevumid <djurayevumid06@gmail.com>
2022-07-21 00:47:00 +0300

    2.1.2
    2.1.2

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat cat1 = applicationContext.getBean("getCat", Cat.class);
+        Cat cat2 = applicationContext.getBean("getCat", Cat.class);
+
+
+        System.out.println(bean == bean2);
+        System.out.println(cat1 == cat2);
+    }
+}
